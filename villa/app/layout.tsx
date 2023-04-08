667841e2f48a76378a31a2c86723a7aa1e1e533c
Allan <altonallan47@gmail.com>
2023-04-08 13:06:19 +0200

    Update Layout

--- villa/app/layout.tsx
+++ villa/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Nunito } from 'next/font/google';
+import Navbar from './components/Navbar/Navbar';
 import './globals.css'
 
 export const metadata = {
@@ -12,12 +13,15 @@ const font = Nunito({
 
 export default function RootLayout({
   children,
-}: {
+}: {   
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+         {children}
+      </body>
     </html>
   )
 }
